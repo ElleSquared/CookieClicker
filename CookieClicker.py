82c27764a5fb2fa35a32af7ee42ba6862dd776ad
ElleSquared <PiesAreCircle@hotmail.com>
2022-01-16 20:50:09 -0700

    Second Stream Progress!!!
    We can click and read???

--- CookieClicker.py
+++ CookieClicker.py
@@ -1,6 +1,8 @@
 #Initialize!!!
 import sys
 import time
+sys.path.insert(0, '../Secret')
+import Location
 
 #### Control Modules ####
 import pyautogui
@@ -8,30 +10,37 @@ from pynput.mouse import Button, Controller
 from pynput import keyboard
 
 #### Image Modules ####
-from PIL import Image
-
-
+from PIL import Image, ImageGrab
+import pytesseract
 
 def on_press(key):
     if key == keyboard.Key.esc:
-         print('esc pressed!')
-         sys.exit()
-         return False # Stop listener
+        print('esc pressed!')
+        storyText.close()
+        sys.exit()
+        return False # Stop listener
 
-listener = keyboard.Listener(on_press=on_press)
-mouse = Controller()
+def Initalize():
+    listener = keyboard.Listener(on_press=on_press)
+    global mouse
+    mouse = Controller()
 
-input('Press Enter to Continue!')
-listener.start()
+    input('Press Enter to Continue!')
+    listener.start()
+
+    ## Find Cookie Location
+    cookie = Image.open('Cookie.PNG')
+    pyautogui.click('Cookie.PNG')
+    global cookiePos
+    cookiePos = pyautogui.position()
+
+    ##Initalize
+    global lastText, storyText
+    storyText = open('story.txt', 'a')
+    lastText = ""
+
 #1. Click Big Cookie
-cookie=Image.open('Cookie.PNG')
-
-## Find Cookie Location
-pyautogui.click('Cookie.PNG')
-cookiePos = pyautogui.position()
-
-while(True):
-    ## Click Cookie
+def clickCookie():
     mouse.position = cookiePos
     mouse.click(Button.left)
 
@@ -39,3 +48,23 @@ while(True):
 #2. Click Golden Cookie 
 #3. Buy Upgrades
 #4. Read Text
+def readStory():
+    global lastText
+    Screen = ImageGrab.grab()
+    Screen = Screen.crop((890, 112, 2100, 200))
+    pixels = [i for i in Screen.getdata()]
+
+    if (245, 246, 247) in pixels: #True if exists, False if it doesn't
+        text = pytesseract.image_to_string(Screen, config='-c page_separator="" --psm 7')
+        if text != lastText :
+            print(text.replace("\n", " "))
+            storyText.write(text)
+            lastText = text
+
+Initalize()
+
+while(True):
+    clickCookie()
+    readStory()
+
+storyText.close()
